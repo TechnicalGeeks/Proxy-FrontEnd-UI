6e38862a9a6b53771f1e042fcd4f4eb2e14cf978
Rohan1904 <51150171+Rohan1904@users.noreply.github.com>
2020-09-03 22:54:51 +0530

    added index.js and initial auth process

---
+++ public/index.js
@@ -0,0 +1,14 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+    } else {
+      // No user is signed in.
+    }
+  });
+
+function login(){
+    var userEmail=document.getElementById("email_field").value;
+    var userPass=document.getElementById("password_field").value;
+
+    window.alert(userEmail+" --  "+ userPass)
+}
