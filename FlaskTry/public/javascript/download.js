e79ce9842e41ad3450b5d4b941c2df77a237ffcd
Rohan1904 <51150171+Rohan1904@users.noreply.github.com>
2020-10-13 20:16:45 +0530

    Added download attendance program

---
+++ FlaskTry/public/javascript/download.js
@@ -0,0 +1,62 @@
+var config = {
+    "apiKey": "AIzaSyAC4EU24xjMQKXP-41I1TnRDIT4KTN7CV8",
+    "authDomain": "proxy-detection-1df22.firebaseapp.com",
+    "databaseURL": "https://proxy-detection-1df22.firebaseio.com",
+    "projectId": "proxy-detection-1df22",
+    "storageBucket": "proxy-detection-1df22.appspot.com",
+    "messagingSenderId": "17187188207",
+    "appId": "1:17187188207:web:63e8c1f5b50862b1c59a1a",
+    "vmeasurementId": "G-EPTQX1DS4L"
+}
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+
+
+document.getElementById('download').onclick = function download_att() {
+    var IClass = document.getElementById('year').value;
+    var IDiv = document.getElementById('div').value;
+    var csv = 'ATTENDANCE REPORT , DIVISION - \n';
+    var data = []
+    var subjects = []
+    document.write("Download Pressed")
+    database.ref('/' + IClass + '/' + IDiv).once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            // working in Year 
+            var stud = []
+            var childKey = childSnapshot.key;
+            document.write("Working  with roll  ")
+            var childData = childSnapshot.val();
+            var sub = Object.values(childData)
+            var sub_name = Object.keys(childData)
+            subjects = sub_name;
+            console.log(sub_name)
+            document.write(childKey + "Subject ttendance " + sub + '<br>')
+            stud.push(childKey)
+            for (index = 0; index < sub.length; index++) {
+                document.write('br>' + sub_name[index] + "-------" + sub[index] + " - - ")
+                stud.push(sub[index])
+            }
+            data.push(stud)
+
+        });
+        document.write("Outside Previous Student" + '<br>' + data)
+        data.forEach(function(row) {
+            csv += row.join(',');
+            csv += "\n";
+            console.log(csv);
+
+        });
+
+        document.write('<br><br><br><br>' + "Hello guys" + '<br><br><br><br>')
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'Attendance.csv';
+        hiddenElement.click();
+    });
+
+}
